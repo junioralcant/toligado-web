d3c18f6dc5c960cf942b14b8484ed2da9a646e3c
junioralcant <junioralcant111@gmail.com>
2020-06-29 10:48:06 -0300

    Create component danger

---
+++ src/components/Danger/index.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import { FiCheck, FiTrash2 } from "react-icons/fi";
+import { ToastContainer, toast } from "react-toastify";
+
+import api from "../../services/api";
+
+import { Card } from "./styles";
+
+const Danger = ({ approved }) => {
+  const [dangers, setDangers] = useState([]);
+
+  useEffect(() => {
+    async function loadDanger() {
+      const response = await api.get("/dangers");
+
+      setDangers(response.data.docs);
+    }
+
+    loadDanger();
+  }, []);
+
+  async function destroy(id) {
+    await api.delete(`/dangers/${id}`);
+
+    const response = await api.get("/dangers");
+
+    setDangers(response.data.docs);
+
+    toast.success("Registro excluído com sucesso!", {
+      autoClose: 3000,
+    });
+  }
+
+  async function approve(id) {
+    await api.put(`/dangers/${id}`, { approved: true });
+
+    const response = await api.get("/dangers");
+
+    setDangers(response.data.docs);
+    toast.success("Registro aprovado!", {
+      autoClose: 3000,
+    });
+  }
+
+  return (
+    <>
+      <ToastContainer />
+
+      {dangers.map(
+        (danger) =>
+          danger.approved === approved && (
+            <Card key={danger._id}>
+              <div className="header">
+                <div>
+                  <strong>{danger.user.name}</strong>
+                  <br />
+                  <small>{danger.user.cpf}</small>
+                </div>
+                <small>{danger.createdAt}</small>
+              </div>
+              <img
+                src={danger.image.url ? danger.image.url : null}
+                alt={danger.location}
+              />
+              <div className="footer">
+                <strong>{danger.location}</strong>
+                <p>{danger.description}</p>
+                <div>
+                  <button
+                    className="checked"
+                    onClick={() => {
+                      if (
+                        window.confirm(
+                          `Deseja realmente aprovar esse registro?`
+                        )
+                      )
+                        approve(danger._id);
+                    }}
+                  >
+                    <FiCheck />
+                  </button>
+                  <button
+                    className="delete"
+                    onClick={() => {
+                      if (
+                        window.confirm(
+                          `Deseja realmente deletar esse registro?`
+                        )
+                      )
+                        destroy(danger._id);
+                    }}
+                  >
+                    <FiTrash2 />
+                  </button>
+                </div>
+              </div>
+            </Card>
+          )
+      )}
+    </>
+  );
+};
+
+export default Danger;
---
+++ src/components/Danger/styles.js
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+
+  width: 300px;
+  background: #fff;
+  box-shadow: 2px 2px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+  padding: 10px;
+  color: #666666;
+
+  .header {
+    width: 100%;
+    display: flex;
+    height: 30px;
+    margin-bottom: 10px;
+    justify-content: space-between;
+  }
+
+  img {
+    width: 280px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+  }
+
+  .footer {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 300px;
+    text-align: center;
+
+    strong {
+      font-size: 15px;
+    }
+
+    p {
+      margin-bottom: 10px;
+    }
+
+    div {
+      display: flex;
+      justify-content: space-between;
+      width: 100px;
+
+      button {
+        background: none;
+        border: none;
+        font-size: 22px;
+      }
+
+      button.checked {
+        color: #53c579;
+
+        &:hover {
+          color: #3e8355;
+        }
+      }
+
+      button.delete {
+        color: #f64646;
+
+        &:hover {
+          color: #bb3939;
+        }
+      }
+    }
+  }
+`;
