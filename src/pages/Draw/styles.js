a4c53703b7466f439633467e561362887a5254b4
junioralcant <junioralcant111@gmail.com>
2022-08-16 10:47:02 -0300

    Add rem value units in Draw

--- src/pages/Draw/styles.js
+++ src/pages/Draw/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   position: relative;
   display: flex;
   justify-content: flex-end;
-  margin-top: 50px;
+  margin-top: 3.12rem;
   width: 100%;
 
   @media (max-width: 1560px) {
@@ -17,35 +17,35 @@ export const Content = styled.div`
   align-items: center;
   justify-content: center;
   flex-wrap: wrap;
-  max-width: 1300px;
-  margin-right: 100px;
+  max-width: 81.25rem;
+  margin-right: 6.25rem;
   width: 100%;
 
   @media (max-width: 1720px) {
-    margin-right: 50px;
+    margin-right: 3.12rem;
   }
 
   @media (max-width: 1620px) {
-    margin-right: 20px;
+    margin-right: 1.25rem;
   }
 
   @media (max-width: 1560px) {
-    max-width: 1000px;
-    margin-left: 200px;
+    max-width: 62.5rem;
+    margin-left: 12.5rem;
   }
 
   @media (max-width: 661px) {
-    margin-top: 20px;
-    margin-left: 20px;
+    margin-top: 1.25rem;
+    margin-left: 1.25rem;
   }
 `;
 
 export const Title = styled.h1`
   z-index: 1;
   position: absolute;
-  top: -60px;
+  top: 3.75rem;
   left: 18%;
 
-  font-size: 25px;
+  font-size: 1.56rem;
   color: #4d4d4d;
 `;
