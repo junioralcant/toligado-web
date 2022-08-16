fd82613f3efa01c409f551261ce503513dc4338e
junioralcant <junioralcant111@gmail.com>
2022-08-16 11:19:43 -0300

    Add rem value units in List Record

--- src/pages/ListRecord/styles.js
+++ src/pages/ListRecord/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   justify-content: flex-end;
-  margin-top: 50px;
+  margin-top: 3.12rem;
   width: 100%;
   height: 100%;
 
@@ -18,21 +18,21 @@ export const Content = styled.div`
   align-items: center;
   justify-content: center;
   flex-wrap: wrap;
-  max-width: 1300px;
-  margin-right: 100px;
+  max-width: 81.25rem;
+  margin-right: 6.25rem;
   width: 100%;
 
   @media (max-width: 1720px) {
-    margin-right: 50px;
+    margin-right: 3.12rem;
   }
 
   @media (max-width: 1620px) {
-    margin-right: 20px;
+    margin-right: 1.25rem;
   }
 
   @media (max-width: 1560px) {
-    max-width: 1000px;
-    margin-left: 200px;
+    max-width: 62.5rem;
+    margin-left: 12.5rem;
   }
 
   @media (max-width: 661px) {
