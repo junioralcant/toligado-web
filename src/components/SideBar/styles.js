d6c3f820f4be74ddb7e18d90f6332309b1951206
junioralcant <junioralcant111@gmail.com>
2022-08-16 10:32:32 -0300

    Add rem value units in Sidebar

--- src/components/SideBar/styles.js
+++ src/components/SideBar/styles.js
@@ -1,18 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Button = styled.button`
-  display: ${(props) => (props.teste ? "relative" : "none")};
+  display: ${(props) => (props.teste ? 'relative' : 'none')};
   top: 0;
   right: 0;
   background: none;
   color: #000;
-  font-size: 32px;
-  margin-top: 10px;
-  margin-left: 10px;
+  font-size: 2rem;
+  margin-top: 0.62rem;
+  margin-left: 0.62rem;
   border: none;
 
   @media (max-width: 600px) {
-    display: ${(props) => (props.teste ? "unset" : "none")} !important;
+    display: ${(props) =>
+      props.teste ? 'unset' : 'none'} !important;
     position: absolute !important;
   }
 `;
@@ -23,9 +24,9 @@ export const Close = styled.button`
   right: 0;
   background: none;
   color: #000;
-  font-size: 32px;
-  margin-top: 10px;
-  margin-left: 10px;
+  font-size: 2rem;
+  margin-top: 0.62rem;
+  margin-left: 0.62rem;
   border: none;
   color: #fff;
 
@@ -37,19 +38,19 @@ export const Close = styled.button`
 
 export const SideNav = styled.div`
   height: 100%;
-  width: 200px;
+  width: 12.5rem;
   position: fixed;
   z-index: 1;
   top: 0;
   left: 0;
   background: #208eeb;
   overflow-x: hidden;
-  padding-top: 30px;
+  padding-top: 1.87rem;
 
   @media (max-width: 1200px) {
     display: flex;
     flex-direction: column;
-    width: 100px;
+    width: 6.25rem;
     align-items: center;
 
     p {
@@ -65,26 +66,26 @@ export const SideNav = styled.div`
       width: 100%;
       justify-content: center;
       span {
-        font-size: 20px;
+        font-size: 1.25rem;
       }
     }
   }
 
   @media (max-width: 600px) {
-    display: ${(props) => (props.teste ? "relative" : "none")};
+    display: ${(props) => (props.teste ? 'relative' : 'none')};
   }
 
   strong {
     display: flex;
-    margin: 20px 0px 40px 0px;
+    margin: 1.25rem 0 2.5rem 0;
     text-transform: uppercase;
     align-items: center;
     justify-content: center;
-    font-size: 15px;
+    font-size: 0.93rem;
     color: #fff;
     span {
-      margin-right: 10px;
-      font-size: 25px;
+      margin-right: 0.62rem;
+      font-size: 1.56rem;
       color: #fbc46a;
     }
 
@@ -95,31 +96,31 @@ export const SideNav = styled.div`
   }
 
   div.companyData {
-    font-size: 13px;
-    margin-left: 10px;
+    font-size: 0.81rem;
+    margin-left: 0.62rem;
     color: #fff;
-    margin-top: -30px;
+    margin-top: -1.87rem;
 
     p {
-      margin-bottom: 10px;
+      margin-bottom: 0.62rem;
     }
   }
 
   div {
-    margin-bottom: 30px;
+    margin-bottom: 1.87rem;
     small {
-      margin: 0px 0px 50px 20px;
-      font-size: 15px;
+      margin: 0px 0px 50px 1.25rem;
+      font-size: 0.93rem;
       color: #a6d2f7;
       text-transform: uppercase;
     }
   }
 
   a {
-    margin: 10px 0px 0px 0px;
-    padding: 15px 0px 15px 20px;
+    margin: 0.62rem 0px 0px 0px;
+    padding: 0.93rem 0px 0.93rem 1.25rem;
     text-decoration: none;
-    font-size: 15px;
+    font-size: 0.93rem;
     color: #a6d2f7;
     display: flex;
 
@@ -128,7 +129,7 @@ export const SideNav = styled.div`
     }
 
     span {
-      margin-right: 25px;
+      margin-right: 1.56rem;
     }
   }
 
@@ -145,11 +146,11 @@ export const SideNav = styled.div`
   button {
     position: absolute;
     bottom: 0;
-    margin-bottom: 5px;
-    margin: 10px 0px 0px 0px;
-    padding: 15px 0px 15px 20px;
+    margin-bottom: 0.32rem;
+    margin: 0.62rem 0 0 0;
+    padding: 0.93rem 0 0.93rem 1.25rem;
     text-decoration: none;
-    font-size: 15px;
+    font-size: 0.93rem;
     color: #f73e57;
     display: flex;
     background: none;
@@ -160,13 +161,13 @@ export const SideNav = styled.div`
     }
 
     span {
-      margin-right: 25px;
+      margin-right: 1.56rem;
     }
   }
 `;
 export const Background = styled.div`
   position: fixed;
-  display: ${(props) => (props.teste ? "block" : "none")};
+  display: ${(props) => (props.teste ? 'block' : 'none')};
   top: 0;
   width: 100%;
   height: 100%;
